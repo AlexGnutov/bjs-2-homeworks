c62d73b48ff135e715cb93ebf37541f1c462188c
Alexey Gnutov <alexgnutov@gmail.com>
2021-06-29 12:51:36 +0300

    HW_2 initial version

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,30 +2,69 @@
 function getArrayParams(arr) {
   let min,max,sum,avg;
 
-  // Ваш код
+  min = 101;
+  max = -101;
+  sum = 0;
 
+  let arrayLength = arr.length;
+
+  for (let i = 0; i < arrayLength; i += 1) {
+    let x = arr[i];
+    if (x > max) {
+      max = x;
+    }
+    if (x < min) {
+      min = x;
+    }
+    sum +=x;
+  }
+
+  avg = (sum / arrayLength).toFixed(2);
+  
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i += 1) {
+    sum += arr[i];
+  }  
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max;
 
-  // Ваш кода
-  // for ...
-  
-  return max
+  max = func(arrOfArr[0]);
+  for (let i = 1; i < arrOfArr.length; i += 1) {
+    let arSum = func(arrOfArr[i]);
+    if (arSum > max) {
+      max = arSum;
+    }
+  } 
+
+  return max;
 }
 
 // Задание 3
 function worker2(arr) {
   // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i += 1) {
+    let x = arr[i];
+    if (x < min) {
+      min = x;
+    }
+    if (x > max) {
+      max = x;
+    }
+  }
+  
+  let minmax = max - min;
+  return minmax;  
 }
