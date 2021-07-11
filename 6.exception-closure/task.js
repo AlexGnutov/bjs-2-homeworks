898202683994853f5efbb64c8868143afe407e14
Alexey Gnutov <alexgnutov@gmail.com>
2021-07-11 22:12:25 +0300

    HW_6 rework 1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -18,50 +18,39 @@ function validateCount(value) {
 //Задача №2
 class Triangle {
     constructor(a, b, c) {
-        if ( (a + b - c > 0) && (a + c - b > 0) && (b + c - a > 0)) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-            this.getPerimeter = function() {
-                return (a + b + c);
-            }
-            this.getArea = function() {
-                let hP = this.getPerimeter() / 2;
-                return +(Math.sqrt(hP * (hP - a) * (hP - b) * (hP - c)).toFixed(3))
-            }
-        } else {
+        if ((a + b - c <= 0) || (a + c - b <= 0) || (b + c - a <= 0)) {
             throw new Error("Треугольник с такими сторонами не существует");
         }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    getPerimeter() {
+        return (this.a + this.b + this.c);
+    }
+    getArea() {
+        let hP = this.getPerimeter() / 2;
+        return +(Math.sqrt(hP * (hP - this.a) * (hP - this.b) * (hP - this.c)).toFixed(3));
     }
 }
 
-class WrongTrianlge {
-    static message = "Ошибка! Треугольник не существует";
-    constructor() {
-        this.getArea = function() {
-            return WrongTrianlge.message;
-        }
-        this.getPerimeter = function() {
-            return WrongTrianlge.message;
-        }   
-    }
+function getArea() {
+    return "Ошибка! Треугольник не существует";
 }
+function getPerimeter() {
+    return "Ошибка! Треугольник не существует";
+}
+
+const wrongTriangle = {getArea, getPerimeter}
 
 function getTriangle(a, b, c) {
     try {
         return new Triangle(a, b, c);
     } catch {
-        return new WrongTrianlge();
+        return wrongTriangle;
     }
 }
 
-console.clear();
-console.log(getTriangle(1,3,3));
-console.log(getTriangle(1,3,3).getArea());
-console.log(getTriangle(1,3,3).getPerimeter());
 
-console.log(getTriangle(100,3,2));
-console.log(getTriangle(100,3,2).getArea());
-console.log(getTriangle(100,3,2).getPerimeter());
 
 
